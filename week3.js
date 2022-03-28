023a90b6a2a8600622f8de6676f89bdeaf4da52f
Chaerim Park <dasapcr@gmail.com>
2022-03-28 16:10:04 +0900

    0328 upload

---
+++ week3.js
@@ -0,0 +1,50 @@
+//변수 : 선언, 초기화, 재할당
+
+const 변수1 = '코딩';
+let 변수2 = '자바스크립트';
+//const는 재할당 불가능
+
+//데이터 타입, 자료형
+// 1. string : 문자열
+const str = '안녕';
+
+//2. number : 숫자
+const number = 3;
+
+//3. bigInt : 큰값인 숫자
+
+//4. boolean : true, false
+const bool = true;
+
+//5. symbol : 중복되지 않는 고유값
+
+//6.7. null / undefined
+const a = undefined; //자동적으로 값이 없음
+const b = null; //수동적으로 값이 없음
+
+//8. object : 객체 - 함수, 배열, 객체
+
+
+//연산자
+//1. 산술연산 : +, -, *, /, %, **
+const c = 20 % 3;
+console.log(c);
+
+const d = 10 ** 2;
+console.log(d);
+
+//2. 할당 연산자 : =
+
+//3. 문자열 연산자 : +, +=
+let data = "happy" + "2022";
+
+data += "!!";
+console.log(data);
+
+//4. 비교연산자 : ==, ===(데이터 타입까지 엄격 비교), !=, !==
+
+null === undefined //false
+
+//5. 논리연산자 : &&, ||, !
+
+
